05877d32af29d90a52173b10feab331e51f1a084
Hugo Reyes <hgamalielbj@gmail.com>
2024-07-30 10:35:55 -0600

    agl-dto Add IssueIterationReportDTO

---
+++ src/main/java/com/agl/metrics/dto/IssueIterationReportDTO.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.agl.metrics.dto;
+
+/**
+ *
+ * @author hugo.reyes
+ */
+public class IssueIterationReportDTO {
+    
+    private String name;
+    private Long points;
+    private Long iterationNumber;
+    
+    public String getName() {
+        return name;
+    }
+
+    public Long getPoints() {
+        return points;
+    }
+
+    public Long getIterationNumber() {
+        return iterationNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPoints(Long points) {
+        this.points = points;
+    }
+
+    public void setIterationNumber(Long iterationNumber) {
+        this.iterationNumber = iterationNumber;
+    }
+}
